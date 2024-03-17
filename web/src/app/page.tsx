8fc9c49566ed101a9f357331dfe5524baecd7162
Denisson Pereira Santos <denisson159@gmail.com>
2024-03-17 00:37:14 -0300

    test: :white_check_mark: Add test with vitest

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,38 +1,10 @@
-'use client'
-
-import { Spin } from '@/model';
-import React, { useState, useEffect } from 'react';
-
-
-const Mag = () => {
-    const [data, setData] = useState<Spin[]>([]); 
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:8080/spin1');
-                const jsonData = await response.json();
-                setData(jsonData);
-            } catch (error) {
-                console.error('Erro ao obter dados da API:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    return (
-        <div>
-            <h2>Dados da API</h2>
-            <ul>
-                {data.map(item => (
-                    <li key={item.id}>
-                        ID: {item.id}, Temperatura: {item.temperatura}, M_A: {item.m_a}, M_B: {item.m_b}, Mag Staggered: {item.mag_staggered}, Mag Total: {item.mag_total}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
-
-export default Mag;
+import Link from 'next/link'
+ 
+export default function Page() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+    </div>
+  )
+}
